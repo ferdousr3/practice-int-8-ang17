a66d1e2f2d765554f823fd0291226c955f04f525
ferdousr3 <ferdousr1992@gmail.com>
2023-12-14 02:09:04 +0600

    add admin routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutes } from './app.routes';
+import { AdminComponent } from './areas/admin/admin.component';
 import { PublicComponent } from './areas/public/public.component';
 import { PageNotFoundComponent } from './areas/public/views/page-not-found/page-not-found.component';
 
@@ -10,6 +11,12 @@ const routes: Routes = [
     component: PublicComponent,
     loadChildren: () =>
       import('./areas/public/public.module').then((m) => m.PublicModule),
+  },
+  {
+    path: AppRoutes.Admin,
+    component: AdminComponent,
+    loadChildren: () =>
+      import('./areas/admin/admin.module').then((m) => m.AdminModule),
   },
   {
     path: '**',
---
+++ src/app/areas/admin/admin-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'dashboard',
+  //   pathMatch: 'full',
+  //   component: DashboardComponent,
+  // },
+  // {
+  //   path: 'profile',
+  //   component: ProfileComponent,
+  // },
+  {
+    path:'admin',
+    children:[
+      {
+        
+        pathMatch:'full',
+        component:DashboardComponent
+      },
+      {
+        path:'profile',
+        component:ProfileComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AdminRoutingModule {}
