6b7b30661ad47fca597148c7099801b003ab2edd
ferdousr3 <ferdousr1992@gmail.com>
2023-12-26 15:14:51 +0600

    add new expression

---
+++ src/app/_core/helpers/regular-expression.helpers.ts
@@ -0,0 +1,4 @@
+export class RegularExpressionHelper {
+  public static readonly Mobile = '^1[3-9]\\d{8}$';
+  public static readonly MobileWithCode = '^(?:\\+88|88)?(01[3-9]\\d{8})$';
+}
--- src/app/areas/sessions/session-5/reactive-forms/reactive-forms.component.ts
+++ src/app/areas/sessions/session-5/reactive-forms/reactive-forms.component.ts
@@ -19,14 +19,18 @@ export class ReactiveFormsComponent {
   registrationForm = this._fromBuilder.group({
     firstName: new FormControl('', [
       Validators.required,
-      Validators.minLength(5),
+      Validators.minLength(4),
+      Validators.maxLength(6),
     ]),
     lastName: new FormControl('', [
       Validators.required,
       Validators.minLength(5),
     ]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    userName: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    userName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
     dob: new FormControl('', [Validators.required, Validators.minLength(5)]),
     gender: new FormControl('male', [Validators.required]),
     country: new FormControl('bangladesh', [Validators.required]),
@@ -34,7 +38,20 @@ export class ReactiveFormsComponent {
     terms: new FormControl('', [Validators.required]),
   });
 
+  get firstName() {
+    return this.registrationForm.get('firstName');
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
+  get lastName() {
+    return this.registrationForm.get('lastName');
+  }
+  get terms() {
+    return this.registrationForm.get('terms');
+  }
+
   onSubmit() {
-    console.log(this.registrationForm.value);
+    console.log(this.registrationForm);
   }
 }
