65ffa1fd6bb304774bef4be76640c6eb470038de
ferdousr3 <ferdousr1992@gmail.com>
2024-01-09 15:03:29 +0600

    add error handling

--- src/app/areas/sessions/session-7/get-data/get-data.component.ts
+++ src/app/areas/sessions/session-7/get-data/get-data.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { catchError, map, throwError } from 'rxjs';
+import { map } from 'rxjs';
 import { IProduct } from './productInterface';
 
 @Component({
@@ -15,11 +15,38 @@ export class GetDataComponent {
   isLoading: boolean = false;
   constructor(private http: HttpClient) {}
   url: string =
-    'https://aninter8-bd319-default-rtdb.firebaseio.com/product.json';
+    'https://aninter8-bd319-default-rtdb.firebaseio.com/product.jsonf';
   getProduct() {
     this.isLoading = true;
     this.http
       .get<{ [key: string]: IProduct }>(this.url)
+      //   .pipe(
+      //     map((res) => {
+      //       const products = [];
+      //       for (const key in res) {
+      //         if (res.hasOwnProperty(key)) {
+      //           products.push({ ...res[key], id: key });
+      //         }
+      //       }
+      //       return products;
+      //     }),
+      //     catchError((err) => {
+      //       return throwError(() => {
+      //         console.log(err.error);
+      //         return err.status;
+      //       });
+      //     })
+      //   )
+      //   .subscribe((products) => {
+      //     this.allProducts = products;
+      //     this.isLoading = !this.isLoading;
+      //   });
+      // // catchError((err) => {
+      // //   return throwError(() => {
+      // //     console.log(err.statusText);
+      // //     return err.status;
+      // //   });
+      // // });
       .pipe(
         map((res) => {
           const products = [];
@@ -31,15 +58,16 @@ export class GetDataComponent {
           return products;
         })
       )
-      .subscribe((products) => {
-        this.allProducts = products;
-        this.isLoading = !this.isLoading;
-      }),
-      catchError((err) => {
-        return throwError(() => {
-          console.log(err);
-          return err;
-        });
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.allProducts = res;
+          this.isLoading = !this.isLoading;
+        },
+        error: (res) => {
+          this.isLoading = !this.isLoading;
+          console.log(res.error);
+        },
       });
   }
 }
