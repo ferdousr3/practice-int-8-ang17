b672256bfe30fc54fe6b133b1f1f997cebc02d53
ferdousr3 <ferdousr1992@gmail.com>
2024-01-10 18:29:32 +0600

    add on view

--- src/app/areas/public/views/home/home.component.ts
+++ src/app/areas/public/views/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { UserSkeletonComponent } from '../../../../shared/component/skeleton/user-skeleton/user-skeleton.component';
+import { SpinnerComponent } from '../../../../shared/component/spinner/spinner.component';
 import { BlogsComponent } from './blogs/blogs.component';
 import { FeatureComponent } from './feature/feature.component';
 import { HeroComponent } from './hero/hero.component';
 import { HomeData, IBlog } from './home.data';
-import { defer } from 'rxjs';
-import { SpinnerComponent } from '../../../../shared/component/spinner/spinner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BlogsComponent, FeatureComponent, HeroComponent,SpinnerComponent],
+  imports: [
+    BlogsComponent,
+    FeatureComponent,
+    HeroComponent,
+    SpinnerComponent,
+    UserSkeletonComponent,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
