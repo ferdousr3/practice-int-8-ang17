90f51828697b5c3a561c5861f42ab57eb02f2f77
ferdousr3 <ferdousr1992@gmail.com>
2024-01-10 16:16:57 +0600

    add new doc

---
+++ src/app/areas/public/views/home/feature/feature.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feature',
+  standalone: true,
+  imports: [],
+  templateUrl: './feature.component.html',
+})
+export class FeatureComponent {
+
+}
--- src/app/areas/public/views/home/home.component.ts
+++ src/app/areas/public/views/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { BlogsComponent } from './blogs/blogs.component';
+import { FeatureComponent } from './feature/feature.component';
+import { HeroComponent } from './hero/hero.component';
 import { HomeData, IBlog } from './home.data';
+import { defer } from 'rxjs';
+import { SpinnerComponent } from '../../../../shared/component/spinner/spinner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BlogsComponent],
+  imports: [BlogsComponent, FeatureComponent, HeroComponent,SpinnerComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
 })
 export class HomeComponent {
   public blogs: IBlog[] = HomeData.blogs;
